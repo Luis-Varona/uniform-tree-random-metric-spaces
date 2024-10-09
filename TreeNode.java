0348f45b6858899290476a11e07f5bc99f7e33a7
Luis Miguel Varona <lbvarona@mta.ca>
2024-10-08 22:52:36 -0300

    Added method to test for graph distance to start metric space simulation

--- TreeNode.java
+++ TreeNode.java
@@ -1,4 +1,6 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -6,12 +8,14 @@ import java.util.ListIterator;
 
 public class TreeNode {
     private final int label;
+    private final int depth;
     private final TreeNode root;
     private final TreeNode parent;
     private final List<TreeNode> children;
     
     public TreeNode(int label, TreeNode root, TreeNode parent) {
         this.label = label;
+        this.depth = root == null ? 0 : parent.getDepth() + 1;
         this.root = root == null ? this : root;
         this.parent = parent;
         this.children =  new LinkedList<>();
@@ -20,6 +24,10 @@ public class TreeNode {
     //
     public int getLabel() {
         return label;
+    }
+    
+    public int getDepth() {
+        return depth;
     }
     
     public TreeNode getRoot() {
@@ -35,10 +43,15 @@ public class TreeNode {
     }
     
     //
-    public int getDepth() {
-        return parent == null ? 0 : parent.getDepth() + 1;
+    public void addChild(TreeNode child) {
+        children.add(child);
     }
     
+    public void removeChild(TreeNode child) {
+        children.remove(child);
+    }
+    
+    //
     public int getOrder() {
         int size = 0;
         Deque<TreeNode> stack = new ArrayDeque<>();
@@ -89,15 +102,6 @@ public class TreeNode {
     }
     
     //
-    public void addChild(TreeNode child) {
-        children.add(child);
-    }
-    
-    public void removeChild(TreeNode child) {
-        children.remove(child);
-    }
-    
-    //
     public boolean hasChild(int labelChild) {
         boolean result = false;
         ListIterator<TreeNode> childrenIterator = children.listIterator();
@@ -118,5 +122,42 @@ public class TreeNode {
         }
         
         return descendant;
+    }
+    
+    //
+    public int distanceFrom(TreeNode node) {
+        if (node.root != root) {
+            throw new IllegalArgumentException("`node` is not in the same tree as `this`.");
+        }
+        
+        TreeNode ancestorSelf = this;
+        TreeNode ancestorNode = node;
+        ArrayList<Integer> ancestorsSelf = new ArrayList<>(Arrays.asList(label));
+        ArrayList<Integer> ancestorsNode = new ArrayList<>(Arrays.asList(node.getLabel()));
+        int distanceSelf = ancestorsSelf.indexOf(node.getLabel());
+        int distanceNode = ancestorsNode.indexOf(label);
+        
+        while (distanceSelf == -1 && distanceNode == -1) {
+            if (ancestorSelf.getDepth() > 0) {
+                ancestorSelf = ancestorSelf.getParent();
+                ancestorsSelf.add(ancestorSelf.getLabel());
+                distanceNode = ancestorsNode.indexOf(ancestorSelf.getLabel());
+            }
+            
+            if (ancestorNode.getDepth() > 0) {
+                ancestorNode = ancestorNode.getParent();
+                ancestorsNode.add(ancestorNode.getLabel());
+                distanceSelf = ancestorsSelf.indexOf(ancestorNode.getLabel());
+            }
+        }
+        
+        if (distanceSelf == -1) {
+            distanceSelf = ancestorsSelf.indexOf(ancestorsNode.get(distanceNode));
+        }
+        else if (distanceNode == -1) {
+            distanceNode = ancestorsNode.indexOf(ancestorsSelf.get(distanceSelf));
+        }
+        
+        return distanceSelf + distanceNode;
     }
 }
--- WolfvilleTester.java
+++ WolfvilleTester.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class WolfvilleTester {
     public static void main(String[] args) {
+        //
         PruferCode code = new PruferCode(new ArrayList<>(List.of(2, 1, 0, 5, 2, 7, 0)));
         TreeNode root = code.getTreeRoot();
         
@@ -21,5 +22,38 @@ public class WolfvilleTester {
         }
         
         System.out.println(adjString);
+        
+        //
+        TreeNode nodeFour = root.findDescendant(4);
+        int distanceRootToRoot = root.distanceFrom(root);
+        int distanceFourToFour = nodeFour.distanceFrom(nodeFour);
+        int distanceRootToFour = root.distanceFrom(nodeFour);
+        
+        System.out.println("Distance from root to root: " + distanceRootToRoot);
+        System.out.println("Distance from node 4 to node 4: " + distanceFourToFour);
+        System.out.println("Distance from root to node 4: " + distanceRootToFour);
+        
+        //
+        TreeNode nodeZero = root.findDescendant(0);
+        TreeNode nodeFive = root.findDescendant(5);
+        int distanceZeroToFive = nodeZero.distanceFrom(nodeFive);
+        int distanceFourToFive = nodeFour.distanceFrom(nodeFive);
+        
+        System.out.println("Distance from node 0 to node 5: " + distanceZeroToFive);
+        System.out.println("Distance from node 4 to node 5: " + distanceFourToFive);
+        
+        //
+        TreeNode nodeTwo = root.findDescendant(2);
+        TreeNode nodeSix = root.findDescendant(6);
+        int distanceTwoToSix = nodeTwo.distanceFrom(nodeSix);
+        
+        System.out.println("Distance from node 2 (root) to node 6: " + distanceTwoToSix);
+        
+        //
+        TreeNode nodeOne = root.findDescendant(1);
+        TreeNode nodeThree = root.findDescendant(3);
+        int distanceOneToThree = nodeOne.distanceFrom(nodeThree);
+        
+        System.out.println("Distance from node 1 to node 3: " + distanceOneToThree);
     }
 }
